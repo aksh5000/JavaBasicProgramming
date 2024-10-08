1669bad0b7813626f2023353e62510f05c82e508
aksh5000 <mail2akshinder@gmail.com>
2024-10-08 14:18:39 +0530

    [Akshinder] ADD:first program

---
+++ PrintThreeNames.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class PrintThreeNames{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter First name ");
+String name1 = sc.nextLine();
+System.out.print("Enter Second name ");
+String name2 = sc.nextLine();
+System.out.print("Enter Third name ");
+String name3 = sc.nextLine();
+System.out.println("hi, "+name1+", "+name2+", "+"and "+name3);
+}
+}
